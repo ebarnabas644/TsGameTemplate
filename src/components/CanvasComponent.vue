712e2d7088856566e557c9a2cf98989f16297e28
ebarnabas <macskabal2000@gmail.com>
2024-07-15 22:18:29 +0200

    Adding basic status component with player position displayed

--- src/components/CanvasComponent.vue
+++ src/components/CanvasComponent.vue
@@ -2,6 +2,7 @@
 import { onMounted, onUnmounted, ref } from 'vue';
 //import { initGame, setRenderer } from '@/core/gameMain';
 import ChatComponent from "@/components/ChatComponent.vue";
+import StatusComponent from "@/components/StatusComponent.vue"
 import {emitCustomEvent} from "@/core/utilities/customEventEmitter";
 //import { io, Socket } from 'socket.io-client'
 
@@ -17,6 +18,7 @@ onMounted(() => {
 <template>
   <div id="pixiContainer"></div>
   <ChatComponent :defaultX="0.05" :defaultY="0.7"></ChatComponent>
+  <StatusComponent :defaultX="0.05" :defaultY="0.04"></StatusComponent>
   <!--<StatusBar :defaultX="0.05" :defaultY="0.05"></StatusBar>
   <Minimap :defaultX="0.90" :defaultY="0.05"></Minimap>-->
 </template>
---
+++ src/components/StatusComponent.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import ModalComponent from "@/components/ModalComponent.vue";
+import { ref, type HTMLAttributes } from 'vue';
+import { networkSystemComponent } from '@/core/gameMain';
+import { reactive } from 'vue';
+//import { usePlayerStatStore } from '@/stores/entity';
+import { inputSystemComponent } from '@/core/gameMain';
+import { useStatusStore } from '@/stores/statusStore'
+
+const chatInput = ref("")
+const chatbox = ref<HTMLElement>()
+const chat: string[] = reactive([])
+const message = ref<HTMLElement>()
+
+const statusStore = useStatusStore()
+
+//const playerStore = usePlayerStatStore()
+
+</script>
+
+<template>
+  <ModalComponent :default-x="0.5" :default-y="0.6" :modalheight="200" :modalwidth="250">
+    <div>
+      X: {{ statusStore.cordinateX }} <br> Y: {{ statusStore.cordinateY }}
+    </div>
+  </ModalComponent>
+</template>
